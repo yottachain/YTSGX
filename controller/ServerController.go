5f4b7dee50d0162a3f8fb4577c9b4a9fbb5a78a5
penghuaisong <306494097@qq.com>
2021-03-05 15:48:57 +0800

    update

--- controller/ServerController.go
+++ controller/ServerController.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"os"
 	"strings"
 
@@ -11,8 +12,8 @@ import (
 //GetInfo test demo
 func GetInfo(g *gin.Context) {
 
-	directory := "/root/mnt/tmp/"
-	fileName := "test.txt"
+	directory := "c:/mnt/tmp/"
+	fileName := "test2.txt"
 	s, err := os.Stat(directory)
 	if err != nil {
 		if !os.IsExist(err) {
@@ -40,4 +41,32 @@ func GetInfo(g *gin.Context) {
 	defer f.Close()
 
 	f.Write([]byte("黄河之水天上来，奔流到海不复回。\r\n"))
+	read(filePath)
+
+}
+
+func read(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	fileinfo, err := file.Stat()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	filesize := fileinfo.Size()
+	buffer := make([]byte, filesize)
+
+	bytesread, err := file.Read(buffer)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("bytes read: ", bytesread)
+	fmt.Println("bytestream to string: ", string(buffer))
 }
