dd4b127e64dd5cef272d9086b6b771c294895c69
penghuaisong <306494097@qq.com>
2021-03-05 17:56:59 +0800

    upload

--- controller/ServerController.go
+++ controller/ServerController.go
@@ -1,6 +1,9 @@
 package controller
 
 import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
 	"fmt"
 	"os"
 	"strings"
@@ -15,7 +18,7 @@ func GetInfo(g *gin.Context) {
 	directory := "/root/mnt/tmp/"
 	fileName := "test3.txt"
 
-	// write(directory, fileName)
+	write(directory, fileName)
 	read(directory, fileName)
 
 }
@@ -47,7 +50,38 @@ func write(directory, fileName string) {
 	}
 	defer f.Close()
 
-	f.Write([]byte("黄河之水天上来，奔流到海不复回。\r\n"))
+	x := []byte("黄河之水天上来，哈哈哈哈哈哈哈哈哈哈哈！！！！！！！！！")
+	key := []byte("hgfedcba87654321")
+	x1 := encryptAES(x, key)
+	f.Write(x1)
+}
+
+func encryptAES(src []byte, key []byte) []byte {
+	block, _ := aes.NewCipher(key)
+	src = padding(src, block.BlockSize())
+	blockmode := cipher.NewCBCEncrypter(block, key)
+	blockmode.CryptBlocks(src, src)
+	return src
+}
+
+func decryptAES(src []byte, key []byte) []byte {
+	block, _ := aes.NewCipher(key)
+	blockmode := cipher.NewCBCDecrypter(block, key)
+	blockmode.CryptBlocks(src, src)
+	src = unpadding(src)
+	return src
+}
+
+func padding(src []byte, blocksize int) []byte {
+	padnum := blocksize - len(src)%blocksize
+	pad := bytes.Repeat([]byte{byte(padnum)}, padnum)
+	return append(src, pad...)
+}
+
+func unpadding(src []byte) []byte {
+	n := len(src)
+	unpadnum := int(src[n-1])
+	return src[:n-unpadnum]
 }
 
 func read(directory, fileName string) {
@@ -79,5 +113,8 @@ func read(directory, fileName string) {
 		return
 	}
 	fmt.Println("bytes read: ", bytesread)
-	fmt.Println("bytestream to string: ", string(buffer))
+
+	key := []byte("hgfedcba87654321")
+	x2 := decryptAES(buffer, key)
+	fmt.Println("bytestream to string: ", string(x2))
 }
